6b93dd8cb1b8a43589e39bdcf96e5b8427a2e686
Lluisaac <isaaclluis@gmail.com>
2021-11-30 12:29:56 +0100

    Initial commit

---
+++ Choco-Pokemon-Team-Builder/src/main/java/Louisaac/Choco_Pokemon_Team_Builder/App.java
@@ -0,0 +1,13 @@
+package Louisaac.Choco_Pokemon_Team_Builder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
